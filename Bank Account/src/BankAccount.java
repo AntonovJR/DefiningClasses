928df12c75210912c39d158a2f6b3de13ebb144b
Alexander Antonov <72743980+AntonovJR@users.noreply.github.com>
2021-04-21 17:35:19 +0300

    Add files via upload

---
+++ Bank Account/src/BankAccount.java
@@ -0,0 +1,37 @@
+class BankAccount {
+    private static double interestRate = 0.02;
+    private static int accountsCount = 0;
+
+    private int id;
+    private double balance;
+
+    public BankAccount() {
+        accountsCount++;
+        this.id = accountsCount;
+        this.balance = 0;
+    }
+
+    public double getInterestRate(int years) {
+        return years * this.getBalance() * interestRate;
+    }
+
+    public static void setInterestRate(double interestRate) {
+        BankAccount.interestRate = interestRate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public double getBalance() {
+        return balance;
+    }
+
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+}
+
+
---
+++ Raw Data/src/Car.java
@@ -0,0 +1,15 @@
+import java.util.Map;
+
+public class Car {
+    private String model;
+    private int engineSpeed;
+    private int enginePower;
+    private int cargoWeight;
+    private String cargoType;
+    private Map<Double, Integer> tyres;
+
+    //The engine,
+    //cargo and tire should be separate classes,
+
+
+}
